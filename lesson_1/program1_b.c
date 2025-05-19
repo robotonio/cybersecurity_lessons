4829cf9db66950c01bd2d78ce4a3a34c4b054301
ROBOTONIO <kasmiris@robotonio.gr>
2025-05-19 10:23:39 +0300

    Update program1_b.c

--- lesson_1/program1_b.c
+++ lesson_1/program1_b.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+#include <string.h>
+
+void vuln() {
+    char buf[32];
+    printf("Δώσε δεδομένα: ");
+    gets(buf);  // επικίνδυνη συνάρτηση
+    printf("Τέλος vuln()\n");
+}
+
+int main() {
+    vuln();
+    return 0;
+}
